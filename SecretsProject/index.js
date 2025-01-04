c26265be862efa63ec1b05898b2c979d893ef753
John <johnprobussr@gmail.com>
2025-01-04 15:07:26 -0600

    practice express with two projects

---
+++ SecretsProject/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// let userIsAuthorized = false;
+
+const passwordCheck = (req, res, next) => {
+  console.log(req.body);
+  const password = req.body['password'];
+  req.userIsAuthorized = password === 'codingGoals';
+  next();
+};
+
+// app.use(passwordCheck);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/check', passwordCheck, (req, res) => {
+  if (req.userIsAuthorized) {
+    res.sendFile(__dirname + '/public/secrets.html');
+  } else {
+    res.sendFile(__dirname + '/public/index.html');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
